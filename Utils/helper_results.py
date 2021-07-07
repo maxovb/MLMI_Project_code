fd6c44bccf61b9a33467f5c602480e0de27b754c
Max-Olivier van Bastelaer <maxovb@gmail.com>
2021-07-07 12:20:52 +0200

    transfer

--- Utils/helper_results.py
+++ Utils/helper_results.py
@@ -295,7 +295,7 @@ def qualitative_evaluation_GP(model, data, num_context_points, num_test_points=1
         if include_class_predictions:
 
             # true kernel probabilities 
-            logpdfs = np.array([f(x_context).logpdf(y_context) for f in data.gps])
+            logpdfs = np.array([f(x_context.detach().cpu().numpy()[0,:,0]).logpdf(y_context.detach().cpu().numpy()[0,:,0]) for f in data.gps])
             logpdfs -= np.max(logpdfs)
             true_probs = np.exp(logpdfs) / np.sum(np.exp(logpdfs))
 
