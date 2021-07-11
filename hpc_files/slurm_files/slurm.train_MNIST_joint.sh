9ab2c3230f8c22a8be7d0841dc4f9f77f9ddf5e7
Max-Olivier Bastelaer <mov22@login-e-12.data.cluster>
2021-07-11 23:26:58 +0100

    first trial GMM hpc

--- hpc_files/slurm_files/slurm.train_MNIST_joint.sh
+++ hpc_files/slurm_files/slurm.train_MNIST_joint.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --time=08:00:00
+#SBATCH --time=14:00:00
 #SBATCH --mail-type=FAIL
 #SBATCH --array=0-7
 #! Uncomment this to prevent the job from being requeued (e.g. if
@@ -20,25 +20,29 @@ source /home/mov22/rds/hpc-work/MLMI_Project_code/hpc_files/activate_environment
 export OMP_NUM_THREADS=1
 
 STEPARRAY=(100 100 100 100 100 100 100 100)
-STEP=${STEPARRAY[$SLURM_ARRAY_TASK_ID]}
 CLARRAY=(False True False True False True False True)
 ETARRAY=(False False True True False False True True)
 GNARRAY=(False False False False True True True True)
+STEP=${STEPARRAY[$SLURM_ARRAY_TASK_ID]}
+CL=${CLARRAY[$SLURM_ARRAY_TASK_ID]}
+ET=${ETARRAY[$SLURM_ARRAY_TASK_ID]}
+GN=${GNARRAY[$SLURM_ARRAY_TASK_ID]}
 
 WDIR=/home/mov22/rds/hpc-work/MLMI_Project_code
-TDIR=$WDIR/hpc_files
+TDIR=${WDIR}/hpc_files
 LOG=${TDIR}/log.$JOBID
 ERR=${TDIR}/stderr.$JOBID
 
 ##
 mkdir -p $TDIR
-JOBID=$SLURM_JOB_ID.$SLURM_ARRAY_TASK_ID
+JOBID=$SLURM_JOB_ID.${SLURM_ARRAY_TASK_ID}
 
 
 echo -e "JobID: $JOBID\n======" > $LOG
 echo "Time: `date`" >> $LOG
 echo "Running on master node: `hostname`" >> $LOG
+echo $CL > $LOG
 
-python $WDIR/train_MNIST_joint.py $STEP --consitencyloss ${CLARRAY[$SLURM_ARRAY_TASK_ID]} --extratask ${ETARRAY[$SLURM_ARRAY_TASK_ID]} --gradnorm ${GNARRAY[$SLURM_ARRAY_TASK_ID]} 
+python $WDIR/train_MNIST_joint.py $STEP --consitencyloss $CL --extratask $ET --gradnorm $GN
 
 
